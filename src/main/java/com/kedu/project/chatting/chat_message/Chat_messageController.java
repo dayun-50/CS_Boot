1ab0036ddf58bb80ce425025f7696ee561de2923
heybin <tlszn10@naver.com>
2025-10-31 12:44:52 +0900

    ㅈ장용

--- src/main/java/com/kedu/project/chatting/chat_message/Chat_messageController.java
+++ src/main/java/com/kedu/project/chatting/chat_message/Chat_messageController.java
@@ -3,6 +3,7 @@ package com.kedu.project.chatting.chat_message;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,11 +15,13 @@ import com.kedu.project.members.member.MemberDTO;
 /*
 	채팅방 메세지 기능 구현 controller
 */
-@RequestMapping("")
+@RequestMapping("/chatMessage")
 @RestController
 public class Chat_messageController {
 	@Autowired
 	private Chat_messageService Chat_messageService;
 	
+//	@PostMapping("")
+//	public ResponseEntity<Chat_messageDTO>
 	
 }
--- src/main/java/com/kedu/project/members/member/MemberService.java
+++ src/main/java/com/kedu/project/members/member/MemberService.java
@@ -27,8 +27,8 @@ public class MemberService {
 
 // JamesAdminClient 주입
 
-//	@Value("${james.local.domain}") 
-//    private String localDomain;
+	@Value("${james.local.domain}") 
+    private String localDomain;
 
 	@Autowired
 	private JamesAdminClient jamesAdminClient;
@@ -46,31 +46,31 @@ public class MemberService {
 		String rawPassword = dto.getPw();
 		
 		// 1. James 계정 이름 생성 (헬퍼 메서드 호출)
-//        String jamesUsername = getJamesUsername(dto.getEmail()); 
+        String jamesUsername = getJamesUsername(dto.getEmail()); 
 
 		// 2. DB 저장을 위해 비밀번호 암호화 및 저장 (member 테이블)
 		dto.setPw(Encryptor.encrypt(dto.getPw()));
-		return dao.signup(dto);
-//		int dbResult = dao.signup(dto); 
-//        
-//        // 3. DB 저장이 성공하면, James 서버에 메일 계정 생성
-//        if (dbResult > 0) {
-//            // James Admin Client 호출 (실패 시 RuntimeException 발생 -> DB 자동 롤백)
-//			jamesAdminClient.createMailAccount(jamesUsername, rawPassword);
-//        }
-//        
-//		return dbResult; 
+//		return dao.signup(dto);
+		int dbResult = dao.signup(dto); 
+        
+        // 3. DB 저장이 성공하면, James 서버에 메일 계정 생성
+        if (dbResult > 0) {
+            // James Admin Client 호출 (실패 시 RuntimeException 발생 -> DB 자동 롤백)
+			jamesAdminClient.createMailAccount(jamesUsername, rawPassword);
+        }
+        
+		return dbResult; 
 	}
 
-//    // 헬퍼 메서드: 이메일에서 ID를 추출하고 James 도메인 결합
-//    private String getJamesUsername(String fullEmail) {
-//        
-//        // 유효성 검사를 React에서 완료했다고 가정하고, @ 앞부분(ID)만 추출
-//        String userId = fullEmail.substring(0, fullEmail.indexOf('@'));
-//        
-//        // 최종 James 계정 이름 반환
-//        return userId + "@" + localDomain;
-//    }
+    // 헬퍼 메서드: 이메일에서 ID를 추출하고 James 도메인 결합
+    private String getJamesUsername(String fullEmail) {
+        
+        // 유효성 검사를 React에서 완료했다고 가정하고, @ 앞부분(ID)만 추출
+        String userId = fullEmail.substring(0, fullEmail.indexOf('@'));
+        
+        // 최종 James 계정 이름 반환
+        return userId + "@" + localDomain;
+    }
 
 	// 로그인
 	public int login(MemberDTO dto) {
