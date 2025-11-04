8f3e1697bcbe7a97cb5807dfe3ad15b6c7ddd8b7
heybin <tlszn10@naver.com>
2025-11-04 11:06:38 +0900

    ㅋㅋ

--- src/main/java/com/kedu/project/interceptor/JwtInterceptors.java
+++ src/main/java/com/kedu/project/interceptor/JwtInterceptors.java
@@ -14,10 +14,40 @@ import jakarta.servlet.http.HttpServletResponse;
 public class JwtInterceptors implements HandlerInterceptor{
 	@Autowired
 	private static JwtUtil jwt;
-//	
-////	@Override
-//	public static String getEmailFromToken(String token)
-//			throws Exception {
-////	
-//	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		String path = request.getRequestURI();
+		String method = request.getMethod();	
+		if(method.equals("OPTIONS")) { // prefilght 보안 패킷은 그냥 허용
+			return true;
+		}
+
+		if(path.equals("/member") || path.equals("/member/login") || path.equals("/member/findpw")
+				|| path.equals("/member/gnewpw")) { // members 와 같은 경우
+			if(method.equals("POST")) {
+				return true;
+			}
+		}
+
+		String authHeader = request.getHeader("Authorization");
+		if(authHeader == null) {
+			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token Error");
+			return false;	
+		}
+		String token = authHeader.substring(7);
+		System.out.println(token);
+
+		try {
+			DecodedJWT djwt = jwt.verifyToken(token);
+			request.setAttribute("email", djwt.getSubject());
+
+			return true;
+		}catch (Exception e) {
+			e.printStackTrace();
+			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token Error");
+			return false;
+		}
+	}
 }
